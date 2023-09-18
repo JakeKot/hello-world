98d0c89811227c21cf4d78eb648a57e26a145f4b
JakeKot <33210096+JakeKot@users.noreply.github.com>
2023-09-18 22:30:45 +0800

    Update Calculate.js

--- Calculate.js
+++ Calculate.js
@@ -1,3 +1,4 @@
+//第一题
 /*Write a function subLength() that takes 2 parameters, 
 a string and a single character. The function should search 
 the string for the two occurrences of the character and return 
@@ -59,3 +60,53 @@ const subLength = (str, char) => {
 
   return len;
 }; //看懂了，但感觉写的还没我好。。。
+
+//第二题
+/*Write a function groceries() that takes an array of object literals of grocery items. The function should return a string with each item separated by a comma except the last two items should be separated by the word 'and'. Make sure spaces (' ') are inserted where they are appropriate.
+
+Examples:
+
+groceries( [{item: 'Carrots'}, {item: 'Hummus'}, {item: 'Pesto'}, {item: 'Rigatoni'}] );
+// returns 'Carrots, Hummus, Pesto and Rigatoni'
+
+groceries( [{item: 'Bread'}, {item: 'Butter'}] );
+// returns 'Bread and Butter'
+
+groceries( [{item: 'Cheese Balls'}] );
+// returns 'Cheese Balls'
+
+*/
+
+//我写的
+const groceries = (obj) => {
+  let grocery = '';
+  if (obj.length > 2) {
+    for (i = 0; i < obj.length - 2; i++) {
+      grocery = grocery + obj[i].item + ', ';
+    }
+    grocery = grocery + obj[obj.length - 2].item + ' and ' +obj[obj.length - 1].item; 
+    return grocery
+  } else if(obj.length === 2) {
+    return obj[0].item + ' and ' + obj[1].item;
+  } else if(obj.length === 1) {
+    return obj[0].item;
+  }
+}
+
+console.log(groceries( [{item: 'Carrots'}] ))
+
+//答案，感觉答案好一点
+const groceries = list => {
+  let listString = ''
+
+  for (let i=0; i<list.length; i++) {
+    listString += list[i].item;
+    if (i < list.length - 2) {
+      listString += ', ';
+    } else if (i == list.length - 2){
+      listString += ' and ';
+    }
+  }
+  
+  return listString;
+}
